7d9f8775bd8062157654d120f5934a74b3aafc33
unixfy <admin@unixfy.me>
2021-12-26 00:32:25 -0500

    Bootstrap a very basic 404 and index

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,4 +1,115 @@
-<h1 class="text-4xl">App?!</h1>
-<p>A short introduction would have been nice here...</p>
+<script>
+    import {tsParticles} from "tsparticles";
+    import {onMount} from "svelte";
+    import Typed from "typed.js";
 
-<a href="/guide">guide</a>
+    let tsparticlesoptions = {
+        particles: {
+            number: {value: 120, density: {enable: true, value_area: 800}},
+            color: {value: "#2196f3"},
+            shape: {
+                type: "circle",
+                stroke: {width: 0, color: "#000000"},
+                polygon: {nb_sides: 5},
+                image: {src: "", width: 100, height: 100},
+            },
+            opacity: {
+                value: 0.75,
+                random: false,
+                anim: {enable: false, speed: 1, opacity_min: 0.1, sync: false},
+            },
+            size: {
+                value: 4.008530152163807,
+                random: true,
+                anim: {enable: false, speed: 40, size_min: 0.1, sync: false},
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#2196f3",
+                opacity: 0.70,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "bounce",
+                bounce: false,
+                attract: {enable: true, rotateX: 600, rotateY: 1200},
+            },
+        },
+        interactivity: {
+            detect_on: "window",
+            events: {
+                onhover: {enable: true, mode: "repulse"},
+                onclick: {enable: true, mode: "bubble"},
+                resize: true,
+            },
+            modes: {
+                grab: {distance: 400, line_linked: {opacity: 1}},
+                bubble: {distance: 400, size: 20, duration: 2, opacity: 8, speed: 3},
+                repulse: {distance: 200, duration: 0.4},
+                push: {particles_nb: 4},
+                remove: {particles_nb: 2},
+            },
+        },
+        retina_detect: true,
+    }
+
+    let typedjsoptions = {
+        strings: ["Self-hosting adventures.", "Technical blogging.", "Bespoke hosting solutions.", "Custom web development.", "Server management.", "AS139921."],
+        typeSpeed: 40,
+        backSpeed: 25,
+        backDelay: 1000,
+        smartBackspace: true,
+        loop: true,
+    }
+
+    onMount(async () => {
+        tsParticles
+            .load("tsparticles", tsparticlesoptions)
+            .then((container) => {
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        let typed = new Typed('.typedjs', typedjsoptions)
+    })
+</script>
+
+<!--Jumbotron-->
+<div class="h-[calc(100vh_-_4rem)] flex">
+    <div class="ct m-auto text-center">
+        <!--        Tsparticle wrapper -->
+        <div id="tsparticles" class="h-100 absolute m-0 p-0 z-[-1]"></div>
+        <img src="/images/logo-cropped.svg" alt="Unixfy" class="md:h-32 lg:h-40 h-20 mx-auto mb-5">
+<!--        <h1 class="md:text-6xl lg:text-7xl text-4xl mb-3">Welcome to unixfy.net</h1>-->
+
+        <!--        TypedJS wrapper -->
+        <div class="sm:text-3xl md:text-4xl lg:text-5xl text-2xl font-mono">
+            <h2 class="typedjs inline"></h2>
+        </div>
+    </div>
+</div>
+
+<div class="h-screen bg-gradient-to-tr from-[#BE93C5] to-[#7BC6CC] text-white">
+    <div class="ct py-8">
+        <h1 class="sm:text-4xl md:text-5xl lg:text-6xl text-3xl">Jump to Apps</h1>
+        <hr>
+    </div>
+</div>
+
+<div class="h-screen bg-red-500 text-white">
+    <h1>Who am I lmao</h1>
+</div>
+
+<div class="h-24 bg-gray-100 flex">
+    <div class="ct my-auto">
+        <p>&copy; 2020-2021 <span class="font-semibold">Unixfy</span>. All rights reserved.</p>
+        <p>Proudly built with <a href="https://svelte.dev" rel="noreferrer" class="link">Svelte</a> and <a href="https://tailwindcss.com" rel="noreferrer" class="link">TailwindCSS</a>. We run on Netlify. &#10084;</p>
+    </div>
+</div>
