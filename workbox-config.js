bd283803ac0bf68b28455daa009c2779b8f049b0
unixfy <admin@unixfy.me>
2021-12-26 15:11:24 -0500

    Maybe fix issue with serviceworker

--- workbox-config.js
+++ workbox-config.js
@@ -2,6 +2,6 @@ module.exports = {
   globDirectory: 'public',
   globPatterns: ['**/*.{js,css,svg}', '__app.html'],
   swSrc: `public/sw.generated.js`,
-  swDest: `dist/sw.generated.js`,
+  swDest: `public/sw.generated.js`,
   maximumFileSizeToCacheInBytes: 10000000, // 10 MB,
 }
