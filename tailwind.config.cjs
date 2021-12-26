1b37bf8e8dec92334c3a4e8ab9835726971c9f4d
unixfy <admin@unixfy.me>
2021-12-26 12:57:18 -0500

    Add a snazzy font pair

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,42 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    extend: {},
-  },
+    theme: {
+        fontFamily: {
+            sans: [
+                '"IBM Plex Sans"',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                '"Noto Sans"',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+                '"Noto Color Emoji"',
+            ],
+            serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+            mono: [
+                'ui-monospace',
+                'SFMono-Regular',
+                'Menlo',
+                'Monaco',
+                'Consolas',
+                '"Liberation Mono"',
+                '"Courier New"',
+                'monospace',
+            ],
+            display: ['"Space Grotesk"', 'ui-sans-serif', 'system-ui']
+        },
+        extend: {},
+    },
 
-  plugins: [],
+    plugins: [],
 };
 
 module.exports = config;
