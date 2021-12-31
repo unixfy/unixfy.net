cf57ee3bc973e9ab0cf01d0f134dcc373289c65a
unixfy <admin@unixfy.me>
2021-12-31 00:34:48 -0500

    Reduce code duplication among categories in AppJumper.svelte

--- src/components/AppJumper.svelte
+++ src/components/AppJumper.svelte
@@ -1,70 +1,53 @@
 <script>
     import Card from "./Card.svelte";
 
-
+    let categories = ["Sites", "Services", "Apps"]
 
     let sites = [
         {
             "title": "tests",
             "content": "test3",
             "link": "#",
-            "icon": "fab fa-atlassian"
+            "icon": "fab fa-atlassian",
+            "category": "Sites"
         },
         {
             "title": "tests",
             "content": "test3",
             "link": "#",
+            "icon": "fab fa-facebook",
+            "category": "Services"
         },
         {
             "title": "tests",
             "content": "test3",
             "link": "#",
+            "icon": "fab fa-google",
+            "category": "Apps"
         },
         {
             "title": "tests",
             "content": "test3",
             "link": "#",
-        },
-
-                {
-            "title": "tests",
-            "content": "test3",
-            "link": "#",
-        },
-                {
-            "title": "tests",
-            "content": "test3",
-            "link": "#",
-        },
-
+            "icon": "fab fa-confluence",
+            "category": "Apps"
+        }
     ]
+
+    let filtersitesbycategory = function (category) {
+        return sites.filter(item => item.category === category)
+    }
 </script>
 
 <div class="h-100">
-    <h1 class="sm:text-2xl md:text-3xl lg:text-4xl text-xl">Sites</h1>
+    {#each categories as category}
+        <h1 class="sm:text-2xl md:text-3xl lg:text-4xl text-xl">{category}</h1>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-4">
-        {#each sites as site}
-            <Card title="{site.title}" content="{site.content}" icon="{site.icon}">
-            </Card>
-        {/each}
-    </div>
-
-    <h1 class="sm:text-2xl md:text-3xl lg:text-4xl text-xl mb-4">Services</h1>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-4">
-        {#each sites as site}
-            <Card title="{site.title}" content="{site.content}">
-            </Card>
-        {/each}
-    </div>
-
-    <h1 class="sm:text-2xl md:text-3xl lg:text-4xl text-xl mb-4">Apps</h1>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-4">
-        {#each sites as site}
-            <Card title="{site.title}" content="{site.content}">
-            </Card>
-        {/each}
-    </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-4">
+            {#each filtersitesbycategory(category) as site}
+                <Card title="{site.title}" content="{site.content}" icon="{site.icon}" link="{site.link}">
+                </Card>
+            {/each}
+        </div>
+    {/each}
 </div>
