5ba4ee2e7be70c8d1ca39a789e7dc07a5053525f
unixfy <admin@unixfy.me>
2021-12-31 19:23:06 -0500

    Bootstrap basic hosting page

---
+++ src/pages/hosting.svelte
@@ -0,0 +1,70 @@
+<script>
+    import PageHeader from "../components/PageHeader.svelte";
+</script>
+
+<div class="ct">
+    <PageHeader title="UnixfyHosting" content="Providing bespoke, rock-solid, awesome hosting services."/>
+
+    <!--    Priicng table-->
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
+            <div>
+                <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Free Plan</h2>
+                <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$0.00</h3>
+            </div>
+
+            <hr class="my-4">
+
+            <ul class="list-inside space-y-2 mb-6 flex-auto">
+                <li>1GB SSD</li>
+                <li>500GB Bandwidth</li>
+                <li>5 Addon domains</li>
+                <li>Unlimited Subdomains</li>
+                <li>ApisCP Control Panel</li>
+                <li><strong>Read terms <a href="https://docs.unixfy.net/free-hosting-offer-4390987.html" target="_blank"
+                                          rel="noreferrer" class="link">here</a></strong></li>
+            </ul>
+
+            <a class="button w-100 block" href="https://my.unixfy.net/order/main/packages/free-shared-hosting/" rel="noreferrer">Order now</a>
+        </div>
+
+        <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
+            <div>
+                <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Coming Soon</h2>
+                <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$???</h3>
+            </div>
+
+            <hr class="my-4">
+
+            <ul class="list-inside space-y-2 mb-6 flex-auto">
+                <li>Something new...</li>
+            </ul>
+
+            <a class="button w-100 block" href="https://my.unixfy.net" rel="noreferrer">Order now</a>
+        </div>
+
+        <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
+            <div>
+                <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Custom Work</h2>
+                <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$&#129300;</h3>
+            </div>
+
+            <hr class="my-4">
+
+            <ul class="list-inside space-y-2 mb-6 flex-auto">
+                <li>Gameservers</li>
+                <li>Web hosting plans</li>
+                <li>Domain registration</li>
+                <li>SSL certs</li>
+                <li>Server management</li>
+                <li><strong>We can help! Get in touch.</strong></li>
+            </ul>
+
+            <a class="button w-100 block" href="#">Contact us</a>
+        </div>
+    </div>
+    <div class="mt-8">
+        <h1 class="text-2xl md:text-3xl">You're already a client?</h1>
+        <p></p>
+    </div>
+</div>
