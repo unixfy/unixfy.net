eaf687f9a420ca6b3521503facf296f2480e58ed
unixfy <admin@unixfy.me>
2021-12-31 23:27:03 -0500

    Externalize tsparticles options to stores

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,120 +1,9 @@
 <script>
     import {tsParticles} from "tsparticles";
-    import {onMount, tick} from "svelte";
     import Typed from "typed.js";
-    import App from "../App.svelte";
     import AppJumper from "../components/AppJumper.svelte";
     import {url} from "@roxi/routify";
-
-    let tsparticlesoptions = {
-        "fullScreen": {
-            "enable": true,
-            "zIndex": -1
-        },
-        "particles": {
-            "number": {
-                "value": 120,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#fff"
-            },
-            "shape": {
-                "type": "circle"
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 10,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": false,
-                "distance": 500,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 2
-            },
-            "move": {
-                "enable": true,
-                "speed": 2,
-                "direction": "bottom",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": true,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "bubble"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "repulse"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 0.5
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 4,
-                    "duration": 0.3,
-                    "opacity": 1,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "detectRetina": true,
-        "fpsLimit": 30,
-        "pauseOnBlur": true,
-        "pauseOnOutsideViewport": true,
-        "background": {
-            "color": "#2196f3",
-            "image": "",
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
-        }
-    }
+    import {tsparticlesoptions} from "../stores";
 
     let typedjsoptions = {
         strings: ["Self-hosting adventures.", "Technical blogging.", "Bespoke hosting solutions.", "Custom web development.", "Server management.", "AS139921."],
@@ -143,7 +32,7 @@
     const mountTsparticles = function () {
         setTimeout(() => {
             tsParticles
-                .load("tsparticles", tsparticlesoptions)
+                .load("tsparticles", $tsparticlesoptions)
                 .catch((error) => {
                     console.error(error);
                 })
