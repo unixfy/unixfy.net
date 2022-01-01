18a59172a5938e93e0ec36db8eb708ab01f2355b
unixfy <admin@unixfy.me>
2022-01-01 17:32:09 -0500

    Fix copyright date

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <div class="h-24 bg-gray-100 dark:bg-gray-800 text-black dark:text-white flex">
     <div class="ct my-auto">
-        <p>&copy; 2020-2021 <span class="font-semibold">Unixfy</span>. All rights reserved.</p>
+        <p>&copy; 2020-{new Date().getFullYear()} <span class="font-semibold">Unixfy</span>. All rights reserved.</p>
         <p>Proudly built with <a href="https://svelte.dev" rel="noreferrer" class="link">Svelte</a> and <a
                 href="https://tailwindcss.com" rel="noreferrer" class="link">TailwindCSS</a>. We run on Netlify.
             &#10084;</p>
