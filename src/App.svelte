6e9032cf2cb88c7f682e2ab165b8144b44f9d169
unixfy <admin@unixfy.me>
2022-05-24 19:55:03 -0400

    Implement basic offline-ready PWA

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
+  import PWAReloadPrompt from "./components/PWAReloadPrompt.svelte";
 </script>
 
 <Router {routes} />
+
+<PWAReloadPrompt />
 
 <style global>
   @tailwind base;
