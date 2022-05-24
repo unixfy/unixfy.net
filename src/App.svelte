e019315c7de1fa459c220007c532f8e756cd739b
unixfy <admin@unixfy.me>
2022-05-24 16:15:00 -0400

    Remove erroneous reference to Serviceworker.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,9 @@
 <script>
   import { Router } from '@roxi/routify'
   import { routes } from '../.routify/routes'
-
-  import Serviceworker from './components/Serviceworker.svelte'
 </script>
 
 <Router {routes} />
-<Serviceworker />
 
 <style global>
   @tailwind base;
