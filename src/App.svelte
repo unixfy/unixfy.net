e667f1ec1f65f667204da5f474aa67a1cd1b46e0
unixfy <admin@unixfy.me>
2022-05-24 20:23:37 -0400

    Serve all Google fonts offline thanks to @fontsource <3

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
 <script>
-  import { Router } from '@roxi/routify'
-  import { routes } from '../.routify/routes'
-  import PWAReloadPrompt from "./components/PWAReloadPrompt.svelte";
+    import {Router} from '@roxi/routify'
+    import {routes} from '../.routify/routes'
+    import PWAReloadPrompt from "./components/PWAReloadPrompt.svelte";
+
+    //  Fonts
+    import "@fontsource/ibm-plex-sans"
+    import "@fontsource/space-grotesk"
 </script>
 
-<Router {routes} />
+<Router {routes}/>
 
-<PWAReloadPrompt />
+<PWAReloadPrompt/>
 
 <style global>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 </style>
