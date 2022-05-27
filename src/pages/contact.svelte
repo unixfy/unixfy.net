699ef935bffa3158979be861f6cbb6d898053282
unixfy <admin@unixfy.me>
2022-05-27 18:31:02 -0400

    Add a really basic contact page

---
+++ src/pages/contact.svelte
@@ -0,0 +1,52 @@
+<script>
+    import PageHeader from "../components/PageHeader.svelte";
+    import {url} from "@roxi/routify";
+</script>
+
+<div class="page-wrapper">
+    <div class="ct">
+        <PageHeader title="Contact"
+                    content="Get in touch if you're interested in learning more or just want to say hi!"/>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div class="border border-blue-500 p-4 rounded">
+                <h2 class="text-2xl md:text-3xl lg:text-4xl font-display mb-4"><i class="fas fa-life-ring"></i>
+                    Support</h2>
+                <p><span class="font-bold">Online support portal: </span> <a
+                        href="https://unixfy.atlassian.net/servicedesk"
+                        rel="noopener" class="link" target="_blank">Open a ticket here</a>
+                </p>
+                <p><span class="font-bold">Email: </span> support [at] unixfy.net</p>
+            </div>
+            <div class="border border-red-500 p-4 rounded">
+                <h2 class="text-2xl md:text-3xl lg:text-4xl font-display mb-4"><i
+                        class="fas fa-exclamation-triangle"></i>
+                    Abuse</h2>
+                <p><span class="font-bold">Online support portal: </span> <a
+                        href="https://unixfy.atlassian.net/servicedesk/customer/portal/6/group/6/create/10026"
+                        rel="noopener" class="link" target="_blank">Open a ticket here</a>
+                </p>
+                <p><span class="font-bold">Email: </span> support [at] unixfy.net</p>
+                <div class="border border-red-500 p-4 rounded mt-4">
+                    <p>You should only use this if you find abuse on a Unixfy
+                        web service, AS139921, or a Unixfy-related domain.</p>
+                    <p class="italic">Response within 24 hours.</p>
+                </div>
+            </div>
+            <div class="border border-green-500 p-4 rounded">
+                <h2 class="text-2xl md:text-3xl lg:text-4xl font-display mb-4"><i class="fas fa-hand-peace"></i>
+                    Personal</h2>
+                <p>Check out this (imaginary) business card:</p>
+                <div class="text-white bg-slate-500 drop-shadow-xl p-4 mt-8 mb-4 hover:scale-105 transition-all">
+                    <p class="font-display text-xl mb-2">Alex</p>
+                    <p><span class="font-bold">Email:</span> alex [at] unixfy.net</p>
+                    <p><span class="font-bold">Discord:</span> <a href="{$url('/discord')}"
+                                                                  class="underline-offset-1 underline">See here</a></p>
+                    <p><span class="font-bold">More: </span> <a href="https://alex-wang.net" rel="noopener"
+                                                                target="_blank" class="underline underline-offset-1">alex-wang.net</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
