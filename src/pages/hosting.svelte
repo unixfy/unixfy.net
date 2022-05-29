dfb922a40de29f728a570f3aabffc7efe58c8e1e
unixfy <admin@unixfy.me>
2022-05-28 23:02:39 -0400

    Componentize the hosting page pricing matrix

--- src/pages/hosting.svelte
+++ src/pages/hosting.svelte
@@ -1,5 +1,6 @@
 <script>
     import PageHeader from "../components/PageHeader.svelte";
+    import HostingPricingMatrix from "../components/HostingPricingMatrix.svelte";
 </script>
 
 <div class="page-wrapper">
@@ -7,65 +8,7 @@
         <PageHeader title="UnixfyHosting" content="Providing bespoke, rock-solid, awesome hosting services."/>
 
         <!--    Priicng table-->
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
-                <div>
-                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Free Plan</h2>
-                    <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$0.00</h3>
-                </div>
-
-                <hr class="my-4">
-
-                <ul class="list-inside space-y-2 mb-6 flex-auto">
-                    <li>1GB SSD</li>
-                    <li>500GB Bandwidth</li>
-                    <li>5 Addon domains</li>
-                    <li>Unlimited Subdomains</li>
-                    <li>ApisCP Control Panel</li>
-                    <li><strong>Read terms <a href="https://docs.unixfy.net/free-hosting-offer-4390987.html"
-                                              target="_blank"
-                                              rel="noreferrer" class="link">here</a></strong></li>
-                </ul>
-
-                <a class="button w-100 block" href="https://my.disulfi.de"
-                   rel="noreferrer">Order now</a>
-            </div>
-
-            <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
-                <div>
-                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Coming Soon</h2>
-                    <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$???</h3>
-                </div>
-
-                <hr class="my-4">
-
-                <ul class="list-inside space-y-2 mb-6 flex-auto">
-                    <li>Something new...</li>
-                </ul>
-
-                <a class="button w-100 block" href="https://my.disulfi.de" rel="noreferrer">Order now</a>
-            </div>
-
-            <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
-                <div>
-                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Custom Work</h2>
-                    <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$&#129300;</h3>
-                </div>
-
-                <hr class="my-4">
-
-                <ul class="list-inside space-y-2 mb-6 flex-auto">
-                    <li>Gameservers</li>
-                    <li>Web hosting plans</li>
-                    <li>Domain registration</li>
-                    <li>SSL certs</li>
-                    <li>Server management</li>
-                    <li><strong>We can help! Get in touch.</strong></li>
-                </ul>
-
-                <a class="button w-100 block" href="https://my.disulfi.de/index.php?fuse=support&controller=ticket&view=submitticket" rel="noreferrer">Contact us</a>
-            </div>
-        </div>
+        <HostingPricingMatrix />
 
         <a href="https://my.disulfi.de/index.php?fuse=home&view=login" rel="noreferrer">
             <div class="mt-8 bg-blue-500 hover:bg-blue-300 p-8 rounded text-white transition">
