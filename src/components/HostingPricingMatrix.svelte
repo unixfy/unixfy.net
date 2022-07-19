473268d567a01209a8587757c03bad26d0722877
Alex <alex@unixfy.net>
2022-07-19 23:14:39 +0000

    Remvoe outdated pricing matrix

--- src/components/HostingPricingMatrix.svelte
+++ src/components/HostingPricingMatrix.svelte
@@ -1,26 +1,4 @@
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
-                <div>
-                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Free Plan</h2>
-<!--                    <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$0.00</h3>-->
-                </div>
-
-                <hr class="my-4">
-
-                <ul class="list-inside space-y-2 mb-6 flex-auto">
-                    <li>1GB SSD</li>
-                    <li>500GB Bandwidth</li>
-                    <li>5 Addon domains</li>
-                    <li>Unlimited Subdomains</li>
-                    <li>ApisCP Control Panel</li>
-                    <li><strong>Read terms <a href="https://docs.unixfy.net/free-hosting-offer-4390987.html"
-                                              target="_blank"
-                                              rel="noreferrer" class="link">here</a></strong></li>
-                </ul>
-
-                <a class="button w-100 block" href="https://my.disulfi.de"
-                   rel="noreferrer">Order now</a>
-            </div>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
 
             <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
                 <div>
@@ -39,7 +17,7 @@
 
             <div class="border border-blue-500 rounded p-8 text-center flex flex-col">
                 <div>
-                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Custom Work</h2>
+                    <h2 class="text-l sm:text-xl md:text-2xl lg:text-3xl mb-2 font-light">Custom</h2>
 <!--                    <h3 class="text-2xl md:text-3xl lg:text-4xl font-display">$&#129300;</h3>-->
                 </div>
 
@@ -49,11 +27,10 @@
                     <li>Gameservers</li>
                     <li>Web hosting plans</li>
                     <li>Domain registration</li>
-                    <li>SSL certs</li>
                     <li>Server management</li>
-                    <li><strong>We can help! Get in touch.</strong></li>
+                    <li><strong>Get in touch.</strong></li>
                 </ul>
 
-                <a class="button w-100 block" href="https://my.disulfi.de/index.php?fuse=support&controller=ticket&view=submitticket" rel="noreferrer">Contact us</a>
+                <a class="button w-100 block" href="https://my.disulfi.de/index.php?fuse=support&controller=ticket&view=submitticket" rel="noreferrer">Contact</a>
             </div>
         </div>
