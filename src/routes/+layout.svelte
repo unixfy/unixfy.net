260576dbd5d4aca2cdb10aa2b1065bf1e3154c5d
unixfy <alex@unixfy.net>
2023-05-26 20:55:28 -0400

    Implement <head> content from Routify site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,28 @@
     import "@fontsource/ibm-plex-sans/500.css"
     import "@fontsource/ibm-plex-sans/600.css"
     import "@fontsource/ibm-plex-sans/700.css"
+    import {page} from "$app/stores";
 </script>
+
+<svelte:head>
+    <title>{(($page.error) ? "Error" : $page.data.title || "Welcome")} | Unixfy.net</title>
+
+    <meta name="theme-color" content="#2196F3"/>
+    <link rel="apple-touch-icon" href="/images/largeicon.png"/>
+    <meta name="twitter:image" content="/assets/img/largeicon.png">
+    <meta name="twitter:description" content="Unixfy.net is the official website for Unixfy.">
+    <meta name="twitter:card" content="summary">
+    <meta name="description" content="Unixfy.net is the official website for Unixfy.">
+    <meta property="og:type" content="website">
+    <meta name="twitter:title" content="Unixfy.net">
+    <meta property="og:image" content="/assets/img/largeicon.png">
+    <link rel="icon" type="image/png" sizes="1000x1000" href="/images/largeicon.png">
+</svelte:head>
+
+<noscript>
+    <div style="background-color: red; padding: 15px;">
+        <p style="color:white; font-size: 2rem;">Please enable JavaScript for the best experience.</p>
+    </div>
+</noscript>
 
 <slot/>
