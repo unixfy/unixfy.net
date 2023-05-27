f734420e32bf36f262d349ffdaf2e10f42fadabd
unixfy <alex@unixfy.net>
2023-05-26 20:42:11 -0400

    Bootstrap new SvelteKit project

--- src/components/PageHeader.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let title;
-    export let content;
-</script>
-
-<div class="border dark:border-gray-500 rounded mb-8 p-8">
-    <h1 class="cta-heading">{title}</h1>
-    <p>{content}</p>
-</div>
