eaba664ff62442ccb69b7c065942e7935acdb4f7
unixfy <alex@unixfy.net>
2023-05-26 20:52:00 -0400

    Implement Fontsource fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-  import "../app.css";
+    import "../app.css";
+
+    //  Fonts
+    import "@fontsource/space-grotesk/400.css"
+    import "@fontsource/space-grotesk/500.css"
+    import "@fontsource/space-grotesk/600.css"
+
+    import "@fontsource/ibm-plex-sans/300.css"
+    import "@fontsource/ibm-plex-sans/400.css"
+    import "@fontsource/ibm-plex-sans/500.css"
+    import "@fontsource/ibm-plex-sans/600.css"
+    import "@fontsource/ibm-plex-sans/700.css"
 </script>
 
-<slot />
+<slot/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        fontFamily: {
+            sans: [
+                '"IBM Plex Sans"',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                '"Segoe UI"',
+                'Roboto',
+                '"Helvetica Neue"',
+                'Arial',
+                '"Noto Sans"',
+                'sans-serif',
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+                '"Noto Color Emoji"',
+            ],
+            serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+            mono: [
+                'ui-monospace',
+                'SFMono-Regular',
+                'Menlo',
+                'Monaco',
+                'Consolas',
+                '"Liberation Mono"',
+                '"Courier New"',
+                'monospace',
+            ],
+            display: ['"Space Grotesk"', 'ui-sans-serif', 'system-ui']
+        },
+        extend: {},
+    },
+    plugins: [],
 }
 
