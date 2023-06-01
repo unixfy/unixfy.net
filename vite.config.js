ae5984a5f530fe49560d6c67ce6a62a2d529040e
unixfy <alex@unixfy.net>
2023-06-01 17:53:16 -0400

    Configure vite-pwa for SvelteKit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,29 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import {SvelteKitPWA} from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), SvelteKitPWA({
+        manifest: {
+            "background_color": "#ffffff",
+            "theme_color": "#7E1F86",
+            "name": "Unixfy.net",
+            "short_name": "Unixfy.net",
+            "start_url": "/",
+            "display": "standalone",
+            "icons": [
+                {
+                    "src": "/images/largeicon.png",
+                    "sizes": "1000x1000",
+                    "type": "image/png",
+                    "purpose": "maskable any"
+                }
+            ]
+        },
+        registerType: 'autoUpdate',
+        strategies: "generateSW",
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
+        },
+    })]
 });
