d19d3cd3b3d449ba85c8ee3c2c4a897f25a9a319
unixfy <alex@unixfy.net>
2023-05-31 22:16:23 -0400

    Fix text colors, show error code on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,18 @@
-<div class="page-wrapper bg-gradient-to-tr from-[#005C97] to-[#363795] text-white bg-animate">
+<script>
+    import {page} from "$app/stores";
+</script>
+
+<div class="page-wrapper bg-gradient-to-tr from-[#005C97] to-[#363795] bg-animate">
     <div class="ct text-center">
-        <div class="mb-16">
-            <h1 class="sm:text-3xl md:text-4xl lg:text-5xl text-2xl font-display font-semibold">Oops! Error 404</h1>
+        <div class="mb-16 text-white">
+            <h1 class="sm:text-3xl md:text-4xl lg:text-5xl text-2xl font-display font-semibold">Oops! {$page.status} Error</h1>
             <h2 class="mt-2">Houston, we've got a problem! We're lost in space!</h2>
         </div>
         <div>
             <img alt="Unixfy's funny 404 illustration" src="/images/404-illustration.svg" width="50%" height="500px"
                  class="mx-auto animate-pulse"/>
             <!--        This is hidden on screens smaller than md -->
-            <p class="text-xs hidden italic md:inline">yep, I'm a great artist :)</p>
+            <p class="text-xs hidden italic md:inline text-white">yep, I'm a great artist :)</p>
         </div>
     </div>
 </div>
